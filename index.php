cc0db74593de102ab98e31cb5fbf3f63e6c42f16
mknapp19 <knappm@optonline.net>
2016-02-03 13:50:07 -0500

    Main Page Added

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+	<div id="main">
+		WASSUP MY NIGGA
+		<!--<form id="search"> -->
+		<div id="search">
+			<input id="searchbox" type="text">
+		</div>
+		<!--</form>-->
+	</div>
+</body>
+<html>
